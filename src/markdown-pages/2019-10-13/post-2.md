9a69ceae086406bc67fa5ac25f7121be0295a5af
Yoonjin <yoon7ks@gmail.com>
2019-10-13 12:36:08 +0900

    test post build

---
+++ src/markdown-pages/2019-10-13/post-2.md
@@ -0,0 +1,7 @@
+---
+path: "/second-post"
+date: "2019-10-13"
+title: "My second blog post"
+---
+
+Hello! 두번째 블로그 입니다.
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,36 @@
 import React from "react"
-import Header from "../component/header"
 import Layout from "../component/layout"
+import { graphql } from "gatsby"
 
-export default () => (
-  <Layout>
-    <h1>hello world</h1>
-    <p>
-      What do I like to do?
-    </p>
-  </Layout>
+const IndexPage = ({ data }) => (
+      <Layout>
+      <h1>hello world</h1>
+      <p>What do I like to do?</p>
+      <h2>Index</h2>
+      {data.allMarkdownRemark.edges.map(post => (
+        <a
+          key={post.node.id}
+          href={post.node.frontmatter.path}>
+          {post.node.frontmatter.title}
+        </a>
+      ))}
+      </Layout>
 )
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(limit: 10) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            path
+          }
+        }
+      }
+    }
+  }
+`
+
+export default IndexPage
